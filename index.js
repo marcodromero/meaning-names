c450e2f792ffb38d6b57d12ce817bf78914cedbc
Marco Romero <marcod.romero@gmail.com>
2025-04-16 17:55:40 -0300

    Aplicar algunos cambios para que sea más claro el codigo

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const supabaseClient = supabase.createClient('https://znymgdnbgpigsheevxtc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpueW1nZG5iZ3BpZ3NoZWV2eHRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4MjA0MDYsImV4cCI6MjA1ODM5NjQwNn0.kIYM6W2b9UrUNrcSLM4yLKiPLDsQYJMXynPUgXryGXE');
 
-let isFetchError = false;
-let names = [];
+const namesList = document.getElementById("names-list");
 let genderChoosed = 'all';
+
+const UI_MESSAGES = {
+  noMatches: "No hubo coincidencias.",
+  databaseError: "La base de datos no está disponible en este momento.",
+  emptySearch: "Escribe un nombre en el buscador.",
+  genderSelectionHint: "Presiona en la casilla H (Hombre) o M (Mujer) para elegir el género del nombre."
+};
 
 async function fetchData(req) {
   const { searchedName, genderChoosed = null } = req.query;
@@ -12,46 +18,32 @@ async function fetchData(req) {
     query = query.eq("gender", genderChoosed);
   }
 
-  let { data: names, error } = await query.like('normalized_name', `${searchedName}%`)
+  let { data, error } = await query.like('normalized_name', `${searchedName}%`)
                                           .order('name', { ascending: true });
 
   if (error) {
-    isFetchError = true;
+    return {error: true, names: []};
   }else {
-    isFetchError = false;
-     return names;
+     return {error: false, names: data};
   }
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-
-  const addResultsToNameList = (arrayNames)=>{
-    const namesList = document.getElementById("names-list");
-    namesList.innerHTML = '';
-    if(arrayNames.length > 0){
-      arrayNames.forEach(dataName => {
-        const option = document.createElement("input");
-        option.type = "checkbox";
-        option.id = 'n'+ dataName.id
-        option.value = dataName.id;
-        option.name = "option";
-        option.style.display = "none";
-        const label = document.createElement("label");
-        label.for = 'n'+ dataName.id;
-        label.innerText = dataName.name;
-        label.appendChild(option); 
-        namesList.appendChild(label);
-      });
-      addEvents();
-    }else{
-      const nesBallon = document.createElement("div");
-      nesBallon.className = "nes-balloon from-left";
-      const nesBallonTextContain = document.createElement("p");
-      const nesBallonText = document.createTextNode("No hubo coincidencias.");
-      nesBallonTextContain.appendChild(nesBallonText);
-      nesBallon.appendChild(nesBallonTextContain);
-      namesList.appendChild(nesBallon);
-    }
+  const addResultsToNameList = (arrayNames, namesList )=>{
+    arrayNames.forEach(dataName => {
+      const option = document.createElement("input");
+      option.type = "checkbox";
+      option.id = 'n'+ dataName.id
+      option.value = dataName.id;
+      option.name = "option";
+      option.style.display = "none";
+      option.dataset.name = dataName.name;
+      option.dataset.meaning = dataName.meaning;
+      const label = document.createElement("label");
+      label.for = 'n'+ dataName.id;
+      label.innerText = dataName.name;
+      label.appendChild(option); 
+      namesList.appendChild(label);
+    });
   }
 
   const addEvents = ()=>{
@@ -61,10 +53,8 @@ async function fetchData(req) {
     const meaningName = document.getElementById("meaning-name");
     options.forEach((option)=>{
       option.addEventListener("change", (e)=>{
-      const nameValue = e.target.value;
-      const nameIndex = names.findIndex((name)=> name.id == nameValue );
-      meaning.innerText = names[nameIndex].meaning;
-      meaningName.innerText = names[nameIndex].name;
+      meaning.innerText = e.target.dataset.meaning;
+      meaningName.innerText = e.target.dataset.name;
     })
     }) 
   } 
@@ -72,94 +62,86 @@ async function fetchData(req) {
   const inputSearch = document.getElementById("input-name");
   inputSearch.addEventListener("input", (e)=> {
     if(!isInputEmpty()){
-      searchName().then((result)=>{
-        if(!isFetchError){
-          result.length > 0 && (names = result);
-          addResultsToNameList(result);
+      searchName(getNormalizeName()).then((result)=>{
+        if(!result.error){
+          if(result.names.length > 0 ){
+            clearNameList(namesList);
+            addResultsToNameList(result.names, namesList );
+            addEvents();
+          }else{
+            showNotification(UI_MESSAGES.noMatches);
+          }
         }else{
-          const namesList = document.getElementById("names-list");
-          namesList.innerHTML = '';
-          const nesBallon = document.createElement("div");
-          nesBallon.className = "nes-balloon from-left";
-          const nesBallonTextContain = document.createElement("p");
-          const nesBallonText = document.createTextNode("La base de datos no está disponible en este momento.");
-          nesBallonTextContain.appendChild(nesBallonText);
-          nesBallon.appendChild(nesBallonTextContain);
-          namesList.appendChild(nesBallon);
+          clearNameList(namesList);
+          showNotification(UI_MESSAGES.databaseError);
         }
-        
     })
     }
-    });
+  });
 
-  const searchName = async ()=>{
-    const searchedName = document.getElementById("input-name").value.toLowerCase().trim();
-      if( genderChoosed !== "none"){
-        searchedName.replace("é", "e")
+  const searchName = async (searchedName)=>{
+    let query = {"searchedName": searchedName};
+
+    if(genderChoosed === "male" || genderChoosed === "female"){
+      query = {...query, genderChoosed}
+    }
+    return await fetchData({query});
+      
+   
+  }
+
+  const getNormalizeName = ()=>{
+    let normalizeName = document.getElementById("input-name").value.toLowerCase().trim();
+    normalizeName.replace("é", "e")
         .replace("í", "i")
         .replace("ó", "o")
         .replace("ú", "u");
-
-        let query = {"searchedName": searchedName};
-
-        if(genderChoosed === "male" || genderChoosed === "female"){
-          query = {...query, genderChoosed}
-        }
-        return await fetchData({query});
-      }
-    return [];
+    return normalizeName;
   }
 
   const isInputEmpty = ()=>{
     const searchedName = document.getElementById("input-name").value.toLowerCase().trim();
-    
-    if(searchedName.length > 0 ){
-      return false;
-    }else{
-    
-      const namesList = document.getElementById("names-list");
-      namesList.innerHTML = '';
-      const nesBallon = document.createElement("div");
-      nesBallon.className = "nes-balloon from-left";
-      const nesBallonTextContain = document.createElement("p");
-      const nesBallonText = document.createTextNode("Escribe un nombre en el buscador.");
-      nesBallonTextContain.appendChild(nesBallonText);
-      nesBallon.appendChild(nesBallonTextContain);
-      namesList.appendChild(nesBallon);
+    if(searchedName.length === 0 ){
+      clearNameList(namesList);
+      showNotification(UI_MESSAGES.emptySearch);
       return true;
     }
+    return false;
   }
 
-
-  const checkFemale = document.getElementById("checkbox-woman");
-  checkFemale.addEventListener("change", (e)=>{
-     setGenderChoosed();
+  const addEventToInputCheckbox = (element)=>{
+    element.addEventListener("change", (e)=>{
+     updateGenderFilter();
      if(!isInputEmpty()){
-       searchName().then((result)=>{
-         if(!isFetchError){
-          result.length > 0 && (names = result);
-          addResultsToNameList(result);
+       if( genderChoosed !== "none"){ 
+        searchName(getNormalizeName()).then((result)=>{
+         if(!result.error){
+          if(result.names.length > 0){
+            clearNameList(namesList);
+            addResultsToNameList(result.names, namesList );
+            addEvents();
+          }else{
+            showNotification(UI_MESSAGES.noMatches, namesList );
+          }
          }
       
-      })
+        })
+       }else{
+        clearNameList(namesList);
+        showNotification(UI_MESSAGES.genderSelectionHint, namesList );
+       }
+       
+    }else{
+      clearNameList(namesList);
+      showNotification(UI_MESSAGES.emptySearch, namesList );
     }
   })
+  }
 
-  const checkMale = document.getElementById("checkbox-man");
-  checkMale.addEventListener("change", (e)=>{
-     setGenderChoosed();
-     if(!isInputEmpty()){
-       searchName().then((result)=>{
-        if(!isFetchError){
-          result.length > 0 && (names = result);
-          addResultsToNameList(result);
-        }
-      
-      })
-    }
-  })
+  addEventToInputCheckbox(document.getElementById("checkbox-woman"));
+  addEventToInputCheckbox(document.getElementById("checkbox-man"));
 
-  const setGenderChoosed = ()=>{
+   const updateGenderFilter = ()=>{
     const isCheckedFemale = document.getElementById("checkbox-woman").checked;
     const isCheckedMale = document.getElementById("checkbox-man").checked;
 
@@ -178,6 +160,20 @@ async function fetchData(req) {
     if(!isCheckedFemale && !isCheckedMale){
       genderChoosed = 'none';
     }
+  }
+
+  const showNotification = (message, namesList )=>{
+    const nesBallon = document.createElement("div");
+    nesBallon.className = "nes-balloon from-left";
+    const nesBallonTextContain = document.createElement("p");
+    const nesBallonText = document.createTextNode(message);
+    nesBallonTextContain.appendChild(nesBallonText);
+    nesBallon.appendChild(nesBallonTextContain);
+    namesList.appendChild(nesBallon);
+  }
+
+  const clearNameList = (namesList)=>{
+    namesList.innerHTML = '';
   }
 
 
